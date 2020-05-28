5911dd5f8d6c0b50affc0c2fc15e2b02e7932943
Ivan Dovzhikov <einegeist@gmail.com>
2020-05-28 13:04:27 +0300

    Add app state interface

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
+export * from './types';
+
 export const cloneDeep = <T extends {} | []>(structure: T): T =>
   JSON.parse(JSON.stringify(structure));
---
+++ src/utils/types.ts
@@ -0,0 +1,5 @@
+import { BoardsState } from 'components/Boards/types';
+
+export interface AppState {
+  boards: BoardsState;
+}
