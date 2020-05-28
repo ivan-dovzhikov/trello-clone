2ee296130a5c316a979ff8303893edda9dfe5d5d
Ivan Dovzhikov <einegeist@gmail.com>
2020-05-28 13:38:46 +0300

    Add boards page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import 'normalize.css';
 import './App.scss';
-import HomePage from 'views/Home';
 import Header from 'layout/Header';
+import HomePage from 'views/Home';
+import BoardsPage from 'views/Boards';
 
 const App: FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: FC = () => {
       <Header />
       <Switch>
         <Route path="/" exact={true} component={HomePage} />
+        <Route path="/boards" component={BoardsPage} />
         <Route path="*" render={() => <h2>No such page</h2>} />
       </Switch>
     </>
---
+++ src/views/Boards/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './styles.scss';
+
+const BoardsPage: FC = () => {
+  return <main className="boards-page"></main>;
+};
+
+export default BoardsPage;
