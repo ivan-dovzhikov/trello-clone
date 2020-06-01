21cea56a44800a6b12a6720b43c3036b7d73051c
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-01 20:38:26 +0300

    Add preventClickDefault function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
+import { MouseEvent } from 'react';
+
 export * from './types';
 
 export const cloneDeep = <T extends {} | []>(structure: T): T =>
   JSON.parse(JSON.stringify(structure));
+
+export const preventClickDefault = (e: MouseEvent) => e.preventDefault();
