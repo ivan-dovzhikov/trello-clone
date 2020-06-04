21946990e782437c6eecbc6321d03e69161d91c6
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-05 02:56:25 +0300

    Add lists action creators

---
+++ src/components/Lists/actions.test.ts
@@ -0,0 +1,50 @@
+import {
+  ListActionTypes,
+  CreateListAction,
+  DeleteListAction,
+  ChangeListAction,
+} from './types';
+import { createList, deleteList, changeList } from './actions';
+const { CREATE_LIST, DELETE_LIST, CHANGE_LIST } = ListActionTypes;
+
+describe('board action creators', () => {
+  it(`should create ${CREATE_LIST} action with generated ID`, () => {
+    const boardId = 'some-board-id';
+    const title = 'New Board';
+
+    const expected: CreateListAction = {
+      type: CREATE_LIST,
+      payload: { boardId, listId: expect.any(String), title },
+    };
+
+    const actual = createList(boardId, title);
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should create ${DELETE_LIST} action`, () => {
+    const boardId = 'some-board-id';
+    const listId = 'some-list-id';
+
+    const expected: DeleteListAction = {
+      type: DELETE_LIST,
+      payload: { boardId, listId },
+    };
+
+    const actual = deleteList(boardId, listId);
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should create ${CHANGE_LIST} action`, () => {
+    const boardId = 'some-board-id';
+    const listId = 'some-list-id';
+    const title = 'New title';
+
+    const expected: ChangeListAction = {
+      type: CHANGE_LIST,
+      payload: { boardId, listId, title },
+    };
+
+    const actual = changeList(boardId, listId, title);
+    expect(actual).toEqual(expected);
+  });
+});
---
+++ src/components/Lists/actions.ts
@@ -0,0 +1,33 @@
+import { v4 } from 'uuid';
+import {
+  ListActionTypes,
+  CreateListAction,
+  DeleteListAction,
+  ChangeListAction,
+} from './types';
+const { CREATE_LIST, DELETE_LIST, CHANGE_LIST } = ListActionTypes;
+
+export const createList = (
+  boardId: string,
+  title: string
+): CreateListAction => ({
+  type: CREATE_LIST,
+  payload: { boardId, listId: v4(), title },
+});
+
+export const deleteList = (
+  boardId: string,
+  listId: string
+): DeleteListAction => ({
+  type: DELETE_LIST,
+  payload: { boardId, listId },
+});
+
+export const changeList = (
+  boardId: string,
+  listId: string,
+  title: string
+): ChangeListAction => ({
+  type: CHANGE_LIST,
+  payload: { boardId, listId, title },
+});
