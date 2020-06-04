4d0bbabe4d6c4a8000d778930b90d6042c178a11
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-05 02:53:27 +0300

    Add lists types

---
+++ src/components/Lists/types.ts
@@ -0,0 +1,46 @@
+export enum ListActionTypes {
+  CREATE_LIST = 'CREATE_LIST',
+  DELETE_LIST = 'DELETE_LIST',
+  CHANGE_LIST = 'CHANGE_LIST',
+}
+const { CREATE_LIST, DELETE_LIST, CHANGE_LIST } = ListActionTypes;
+
+export interface CreateListAction {
+  type: typeof CREATE_LIST;
+  payload: {
+    boardId: string;
+    listId: string;
+    title: string;
+  };
+}
+
+export interface DeleteListAction {
+  type: typeof DELETE_LIST;
+  payload: {
+    boardId: string;
+    listId: string;
+  };
+}
+
+export interface ChangeListAction {
+  type: typeof CHANGE_LIST;
+  payload: {
+    boardId: string;
+    listId: string;
+    title: string;
+  };
+}
+
+export type ListActions =
+  | CreateListAction
+  | DeleteListAction
+  | ChangeListAction;
+
+export interface List {
+  id: string;
+  title: string;
+}
+
+export interface ListsState {
+  [prop: string]: List;
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,9 @@
 import { BoardsState, BoardActions } from 'components/Boards/types';
+import { ListsState, ListActions } from 'components/Lists/types';
 
 export interface AppState {
   boards: BoardsState;
+  lists: ListsState;
 }
 
-export type AllActions = BoardActions;
+export type AllActions = BoardActions | ListActions;
