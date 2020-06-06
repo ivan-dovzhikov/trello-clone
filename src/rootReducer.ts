f1121eaeb563557fadcaaa7cbf7d973a618b15e5
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-06 22:00:54 +0300

    Add card reducer to root reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import boardReducer from 'components/Boards/reducer';
 import listReducer from 'components/Lists/reducer';
+import cardReducer from 'components/Cards/reducer';
 
 export default combineReducers({
   boards: boardReducer,
   lists: listReducer,
+  cards: cardReducer,
 });
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,11 @@
 import { BoardsState, BoardActions } from 'components/Boards/types';
 import { ListsState, ListActions } from 'components/Lists/types';
+import { CardsState, CardActions } from 'components/Cards/types';
 
 export interface AppState {
   boards: BoardsState;
   lists: ListsState;
+  cards: CardsState;
 }
 
-export type AllActions = BoardActions | ListActions;
+export type AllActions = BoardActions | ListActions | CardActions;
