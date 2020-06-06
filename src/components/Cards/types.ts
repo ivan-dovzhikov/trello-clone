57a1e7222852de6fd9acc1c335dd2bb831d41fb0
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-06 21:40:00 +0300

    Add cards types

---
+++ src/components/Cards/types.ts
@@ -0,0 +1,45 @@
+export enum CardActionTypes {
+  CREATE_CARD = 'CREATE_CARD',
+  DELETE_CARD = 'DELETE_CARD',
+  CHANGE_CARD = 'CHANGE_CARD',
+}
+const { CREATE_CARD, DELETE_CARD, CHANGE_CARD } = CardActionTypes;
+
+export interface CreateCardAction {
+  type: typeof CREATE_CARD;
+  payload: {
+    listId: string;
+    cardId: string;
+    content: string;
+  };
+}
+
+export interface DeleteCardAction {
+  type: typeof DELETE_CARD;
+  payload: {
+    listId: string;
+    cardId: string;
+  };
+}
+
+export interface ChangeCardAction {
+  type: typeof CHANGE_CARD;
+  payload: {
+    cardId: string;
+    content: string;
+  };
+}
+
+export type CardActions =
+  | CreateCardAction
+  | DeleteCardAction
+  | ChangeCardAction;
+
+export interface Card {
+  id: string;
+  content: string;
+}
+
+export interface CardsState {
+  [prop: string]: Card;
+}
