85cb549fd69504f6ee306639fd557628d92f872e
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-07 18:12:08 +0300

    Add redux thunk
    App state doesn't have asynchronous actions, but in order to list
    reducer could delete lists attached to deleting board, the DELETE_BOARD
    action creator have to get board lists IDs from state

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,16 @@
-import { Store, createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { Store, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 import { AppState, AllActions } from 'utils';
 import rootReducer from 'rootReducer';
 import { loadState } from 'localStorage';
 
+const middleware = [ReduxThunk];
+
 const store: Store<AppState, AllActions> = createStore(
   rootReducer,
   loadState(),
-  devToolsEnhancer({})
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
