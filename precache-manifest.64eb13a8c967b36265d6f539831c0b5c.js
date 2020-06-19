7d4e7439beebba1df735c3cc3c8aaaabb32552c3
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-20 00:50:47 +0300

    Updates

---
+++ precache-manifest.64eb13a8c967b36265d6f539831c0b5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1a1e6d75de1f2c3974b3cd82a8b4d1",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "8d42f88ab9a4a3126132",
+    "url": "/trello-clone/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0d17812e27364586bf94",
+    "url": "/trello-clone/static/css/main.b7d35a6e.chunk.css"
+  },
+  {
+    "revision": "8d42f88ab9a4a3126132",
+    "url": "/trello-clone/static/js/2.67f99dd6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trello-clone/static/js/2.67f99dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d17812e27364586bf94",
+    "url": "/trello-clone/static/js/main.78cdc6f5.chunk.js"
+  },
+  {
+    "revision": "d8bd4e74a827c24ccec2",
+    "url": "/trello-clone/static/js/runtime-main.ab25ca4b.js"
+  },
+  {
+    "revision": "6ec50973480b49f71412bc6b5062cb2a",
+    "url": "/trello-clone/static/media/bg.6ec50973.jpg"
+  }
+]);
