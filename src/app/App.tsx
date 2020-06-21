9f3c0b00caf3a097576b6b37101ac80a528bcc29
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-21 15:46:59 +0300

    Relocate router and store provider to index.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,34 @@
 import React, { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import 'normalize.css';
 import './App.scss';
-import store from './store';
-import { watchState } from './localStorage';
 import Header from './Header/Header';
 import HomePage from './HomePage/HomePage';
 import BoardPage from './BoardPage/BoardPage';
 import BoardsNavigation from 'boards';
 import NotFoundPage from './NotFoundPage/NotFoundPage';
 
-watchState();
-
 const App: FC = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Provider store={store}>
-        <Header />
-        <Switch>
-          <Route path="/" exact={true} component={HomePage} />
-          <Route
-            path="/boards"
-            render={() => (
-              <>
-                <BoardsNavigation />
-                <Route path="/boards/:id" component={BoardPage} />
-              </>
-            )}
-          />
-          <Route
-            path="*"
-            render={() => <NotFoundPage message="Such page doesn't exist!" />}
-          />
-        </Switch>
-      </Provider>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact={true} component={HomePage} />
+        <Route
+          path="/boards"
+          render={() => (
+            <>
+              <BoardsNavigation />
+              <Route path="/boards/:id" component={BoardPage} />
+            </>
+          )}
+        />
+        <Route
+          path="*"
+          render={() => <NotFoundPage message="Such page doesn't exist!" />}
+        />
+      </Switch>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from 'app/App';
+import store from 'app/store';
+import { watchState } from 'app/localStorage';
+
+watchState();
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById('app')
 );
