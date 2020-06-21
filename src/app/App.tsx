fa5fe3e8352c6733ffdc6714cb7c29201d092a34
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-21 15:48:44 +0300

    Add Intl provider to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import { IntlProvider } from 'react-intl';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import 'normalize.css';
 import './App.scss';
+import { AppState } from 'utils';
+import { AvailableLanguages } from './localization/types';
+import translations from './localization/data';
 import Header from './Header/Header';
 import HomePage from './HomePage/HomePage';
 import BoardPage from './BoardPage/BoardPage';
@@ -9,8 +14,12 @@ import BoardsNavigation from 'boards';
 import NotFoundPage from './NotFoundPage/NotFoundPage';
 
 const App: FC = () => {
+  const locale = useSelector<AppState, AvailableLanguages>(
+    ({ locale }) => locale.languageCode
+  );
+
   return (
-    <>
+    <IntlProvider locale={locale} messages={translations[locale].data}>
       <Header />
       <Switch>
         <Route path="/" exact={true} component={HomePage} />
@@ -28,7 +37,7 @@ const App: FC = () => {
           render={() => <NotFoundPage message="Such page doesn't exist!" />}
         />
       </Switch>
-    </>
+    </IntlProvider>
   );
 };
 
