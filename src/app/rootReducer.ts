06e525dd3067df208d2d0ebb66f10ed46f6fcfb2
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-21 15:42:26 +0300

    Connect localization to redux

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import localeReducer from './localization/reducer';
 import boardReducer from 'boards/reducer';
 import listReducer from 'lists/reducer';
 import cardReducer from 'cards/reducer';
 
 export default combineReducers({
+  locale: localeReducer,
   boards: boardReducer,
   lists: listReducer,
   cards: cardReducer,
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,17 @@
+import { LocaleState, LocaleActions } from 'app/localization/types';
 import { BoardsState, BoardActions } from 'boards/types';
 import { ListsState, ListActions } from 'lists/types';
 import { CardsState, CardActions } from 'cards/types';
 
 export interface AppState {
+  locale: LocaleState;
   boards: BoardsState;
   lists: ListsState;
   cards: CardsState;
 }
 
-export type AllActions = BoardActions | ListActions | CardActions;
+export type AllActions =
+  | LocaleActions
+  | BoardActions
+  | ListActions
+  | CardActions;
