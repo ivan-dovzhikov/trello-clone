e14bc5c048c7691f777785228247a3894d17cf7e
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-23 15:50:36 +0300

    Add closestByReference function

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -5,3 +5,14 @@ export const preventClickDefault = (e: MouseEvent | ReactMouseEvent) =>
 
 export const cloneDeep = <T extends {} | []>(structure: T): T =>
   JSON.parse(JSON.stringify(structure));
+
+export const closestByReference = (element: HTMLElement, ref: HTMLElement) => {
+  let currentElement: HTMLElement | Node | null = element;
+
+  do {
+    if (currentElement === ref) return currentElement;
+    currentElement = currentElement.parentElement || currentElement.parentNode;
+  } while (currentElement !== null && currentElement.nodeType === 1);
+
+  return null;
+};
