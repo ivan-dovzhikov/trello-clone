3436b97b9b0f6b528e5334151c90ef9cdd9afaa9
Ivan Dovzhikov <einegeist@gmail.com>
2020-06-29 18:27:22 +0300

    Fix Theme toggle label

--- src/app/Header/Menu/ThemeToggle.tsx
+++ src/app/Header/Menu/ThemeToggle.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useIntl } from 'react-intl';
 import { AppState } from 'app/types';
 import { toggleTheme as toggleThemeActionCreator } from 'themes/actions';
 import { Toggle } from 'shared';
 import './ThemeToggle.scss';
 
 const ThemeToggle: FC = () => {
+  const intl = useIntl();
   const dispatch = useDispatch();
 
   const currentTheme = useSelector<AppState>(({ theme }) => theme.theme);
@@ -13,7 +15,8 @@ const ThemeToggle: FC = () => {
 
   return (
     <label>
-      Change theme
+      {intl.formatMessage({ id: 'app/change-theme', defaultMessage: 'Theme' })}
+      {':'}
       <div className="theme-toggle__toggle-container">
         <Toggle checked={currentTheme === 'dark'} onChange={toggleTheme} />
       </div>
